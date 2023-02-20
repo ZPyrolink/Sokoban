29afb216c56a6ef3bcb25d824ec9747b02290f9a
pyrolink <38cz74@gmail.com>
2023-02-20 23:44:46 +0100

    Add log settings

--- src/Global/Logger.java
+++ src/Global/Logger.java
@@ -7,26 +7,31 @@ import java.util.List;
 public final class Logger
 {
     private static final Logger instance = new Logger();
+
     public static Logger getInstance()
     {
         return instance;
     }
 
     private final List<PrintStream> traceListeners;
+
     public void addTraceLsn(PrintStream stream)
     {
         traceListeners.add(stream);
     }
+
     public void clearTraceLsn()
     {
         traceListeners.clear();
     }
 
     private final List<PrintStream> errorListeners;
+
     public void addErrorLsn(PrintStream stream)
     {
         errorListeners.add(stream);
     }
+
     public void clearErrorLsn(PrintStream stream)
     {
         errorListeners.clear();
@@ -47,27 +52,32 @@ public final class Logger
 
     public void log(String log)
     {
-        traceListeners.forEach(tl -> tl.println(log));
+        if (Settings.logLevel.hasFlag(Settings.LogLevel.Log))
+            traceListeners.forEach(tl -> tl.println(log));
     }
 
     public void logErr(String log)
     {
-        errorListeners.forEach(el -> el.println(log));
+        if (Settings.logLevel.hasFlag(Settings.LogLevel.Log))
+            errorListeners.forEach(el -> el.println(log));
     }
 
     public void throwException(Exception e, String message, int exitCode)
     {
-        errorListeners.forEach(el ->
-        {
-            el.println(message);
-            e.printStackTrace(el);
-        });
+        if (Settings.logLevel.hasFlag(Settings.LogLevel.Error))
+            errorListeners.forEach(el ->
+            {
+                el.println(message);
+                e.printStackTrace(el);
+            });
+
         System.exit(exitCode);
     }
 
     public void throwException(Exception e, int exitCode)
     {
-        errorListeners.forEach(e::printStackTrace);
+        if (Settings.logLevel.hasFlag(Settings.LogLevel.Error))
+            errorListeners.forEach(e::printStackTrace);
         System.exit(exitCode);
     }
 }
---
+++ src/Global/Settings.java
@@ -0,0 +1,49 @@
+package Global;
+
+public final class Settings
+{
+    public static LogLevel logLevel = LogLevel.All;
+
+    public static final class LogLevel
+    {
+        public static final LogLevel None = new LogLevel(0),
+                Error = new LogLevel(1 << 1),
+                Log = new LogLevel(1 << 2),
+                All = new LogLevel(Integer.MAX_VALUE);
+
+        private final int value;
+
+        private LogLevel(int value)
+        {
+            this.value = value;
+        }
+
+        public boolean hasFlag(LogLevel flag)
+        {
+            return (value & flag.value) == flag.value;
+        }
+
+        public LogLevel addFlag(LogLevel flag)
+        {
+            return new LogLevel(value | flag.value);
+        }
+
+        public LogLevel removeFlag(LogLevel flag)
+        {
+            return new LogLevel(value & ~flag.value);
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o)
+                return true;
+
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            LogLevel logLevel = (LogLevel) o;
+            return value == logLevel.value;
+        }
+    }
+}
