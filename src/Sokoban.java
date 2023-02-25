a90fbcdb5b9f749c73bc4bc32cf70c2844882fd7
pyrolink <38cz74@gmail.com>
2023-02-25 15:02:37 +0100

    Refactor resources

--- src/Sokoban.java
+++ src/Sokoban.java
@@ -1,6 +1,7 @@
 import GUI.GraphicalInterface;
 import GameSystem.Game;
 import Utils.Logger;
+import Utils.Resource;
 
 import javax.swing.*;
 import java.io.FileInputStream;
@@ -10,15 +11,8 @@ public class Sokoban
 {
     public static void main(String[] args)
     {
-        String levelsPath = args.length >=1 ? args[0] : "res/levels.txt";
-        try
-        {
-            Game.setGame(new Game(new FileInputStream(levelsPath)));
-        }
-        catch (FileNotFoundException e)
-        {
-            Logger.getInstance().throwException(e, "The levels file " + levelsPath + " doesn't exists!", -1);
-        }
+        String levelsName = args.length >=1 ? args[0] : "originals";
+        Game.setGame(new Game(Resource.Game.open(levelsName)));
         Game.getGame().next();
         System.out.println(Game.getGame().currentLevel());
 
--- src/Utils/Resource.java
+++ src/Utils/Resource.java
@@ -1,7 +1,8 @@
 package Utils;
 
+import GameSystem.Game;
+
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,7 +10,8 @@ import java.io.InputStream;
 
 public enum Resource
 {
-    Image("Images", "png");
+    Image("Images", "png"),
+    Game("Levels", "txt");
 
     private final String path;
     private final String extension;
@@ -47,17 +49,24 @@ public enum Resource
     @SuppressWarnings("unchecked")
     public <T> T load(String name)
     {
-        try
+        try (InputStream stream = open(name))
         {
-            return switch (this)
-                    {
-                        case Image -> (T) ImageIO.read(Resource.Image.open(name));
-                    };
+            switch (this)
+            {
+                case Image:
+                    return (T) ImageIO.read(stream);
+                case Game:
+                    return (T) new Game(stream);
+                default:
+                    Logger.getInstance()
+                            .throwException(new RuntimeException("Error loading Resource"), 4);
+            }
         }
         catch (IOException e)
         {
-            Logger.getInstance().throwException(e, "The image " + name + " doesn't exists!", -2);
-            return null;
+            Logger.getInstance().throwException(e, "Error loading " + name + " " + this + " resource!", -2);
         }
+
+        return null;
     }
 }
