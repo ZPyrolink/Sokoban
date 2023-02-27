116db12263bd3664c8546a6237eb6d988b21dba5
pyrolink <38cz74@gmail.com>
2023-02-27 20:12:51 +0100

    Refactor and document Logger

--- src/Managers/Logger.java
+++ src/Managers/Logger.java
@@ -6,67 +6,98 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Utility class for logs */
 @UtilityClass
 public class Logger
 {
+    /** Listeners used for logs */
     private final List<PrintStream> traceListeners = new ArrayList<>()
     {{
         add(System.out);
     }};
 
+    /** Add a listener on {@link #traceListeners} */
     public void addTraceLsn(PrintStream stream)
     {
         traceListeners.add(stream);
     }
-
+    /** Clear the {@link #traceListeners} */
     public void clearTraceLsn()
     {
         traceListeners.clear();
     }
 
+    /** Listeners used for error */
     private final List<PrintStream> errorListeners = new ArrayList<>()
     {{
         add(System.err);
     }};
 
+    /** Add a listener on {@link #errorListeners} */
     public void addErrorLsn(PrintStream stream)
     {
         errorListeners.add(stream);
     }
-
-    public void clearErrorLsn(PrintStream stream)
+    /** Clear the {@link #errorListeners} */
+    public void clearErrorLsn()
     {
         errorListeners.clear();
     }
 
+    /** Log a message */
     public void log(String log)
     {
         if (Settings.logLevel.hasFlag(Settings.LogLevel.Log))
             traceListeners.forEach(tl -> tl.println(log));
     }
 
+    /** Log an error */
     public void logErr(String log)
     {
         if (Settings.logLevel.hasFlag(Settings.LogLevel.Log))
             errorListeners.forEach(el -> el.println(log));
     }
 
+    /**
+     * Log a message
+     * @param level Level of the log
+     * @param log Message to log
+     */
+    public void log(Settings.LogLevel level, String log)
+    {
+        if (level.hasFlag(Settings.LogLevel.Log))
+            log(log);
+
+        if (level.hasFlag(Settings.LogLevel.Error))
+            logErr(log);
+    }
+
+    /**
+     * Log a message, print an exception and exit the app
+     * @param e Exception to "throw"
+     * @param message Additive error log
+     * @param exitCode Return code of the app
+     */
     public void throwException(Exception e, String message, int exitCode)
     {
         if (Settings.logLevel.hasFlag(Settings.LogLevel.Error))
             errorListeners.forEach(el ->
             {
-                el.println(message);
+                if (message != null)
+                    el.println(message);
                 e.printStackTrace(el);
             });
 
         System.exit(exitCode);
     }
 
+    /**
+     * Print an exception and exit the app
+     * @param e Exception to "throw"
+     * @param exitCode Return code of the app
+     */
     public void throwException(Exception e, int exitCode)
     {
-        if (Settings.logLevel.hasFlag(Settings.LogLevel.Error))
-            errorListeners.forEach(e::printStackTrace);
-        System.exit(exitCode);
+        throwException(e, null, exitCode);
     }
 }
