b76c814131f392d5528680fa41457ef4b0667137
pyrolink <38cz74@gmail.com>
2023-02-27 21:56:54 +0100

    Comment and refactor GuiUtils

--- src/Utils/GuiUtils.java
+++ src/Utils/GuiUtils.java
@@ -5,15 +5,45 @@ import lombok.experimental.UtilityClass;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Class utility for swing GUI
+ */
 @UtilityClass
 public class GuiUtils
 {
+    /**
+     * Returns the root component for the current component tree.
+     *
+     * @param c   The component
+     * @param <T> Type of the cast root
+     * @return the first ancestor of c that's a Window
+     * @throws RuntimeException the last ancestor is an Applet
+     */
     @SuppressWarnings("unchecked")
     public <T extends Window> T getRoot(Component c)
     {
-        return (T) SwingUtilities.getRoot(c);
+        return (T) getWindow(c);
     }
 
+    /**
+     * Returns the Windows component for the current component tree.
+     *
+     * @param c The component
+     * @return the first ancestor of c that's a Window or the last Applet ancestor
+     * @throws RuntimeException the last ancestor is an Applet
+     */
+    public Window getWindow(Component c)
+    {
+        Component result = SwingUtilities.getRoot(c);
+        if (result instanceof Window w)
+            return w;
+
+        throw new RuntimeException();
+    }
+
+    /**
+     * Set a windows on full screen
+     */
     public void setFullScreen(Window window)
     {
         GraphicsEnvironment.getLocalGraphicsEnvironment()
