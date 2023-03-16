9f7485cfe0019170f9dfa0192f351f780aaf849f
pyrolink <38cz74@gmail.com>
2023-03-16 23:13:51 +0100

    Update animation test

--- src/AnimationTest.java
+++ src/AnimationTest.java
@@ -2,14 +2,26 @@ import Managers.Resource;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 public class AnimationTest
 {
+    private static double fpsToMsPF(int fps)
+    {
+        return TimeUnit.SECONDS.toMillis(1) / (double) fps;
+    }
+
+    private static Image[] sprites;
+    private static int index;
+
     public static void main(String[] args)
     {
-        Image[] sprites = new Image[]
+        sprites = new Image[]
                 {
-                        Resource.Image.load("Pousseur/Pousseur_0_0")
+                        Resource.Image.load("Pousseur/Pousseur_0_0"),
+                        Resource.Image.load("Pousseur/Pousseur_0_1"),
+                        Resource.Image.load("Pousseur/Pousseur_0_2"),
+                        Resource.Image.load("Pousseur/Pousseur_0_3")
                 };
 
         SwingUtilities.invokeLater(() ->
@@ -27,13 +39,24 @@ public class AnimationTest
 
                     g.clearRect(0, 0, width, height);
 
-                    g.drawImage(sprites[0], 0, 0, 100, 100, null);
+                    g.drawImage(sprites[index], 0, 0, 100, 100, null);
                 }
             };
 
             frame.add(comp);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
             frame.setVisible(true);
+
+            Timer t = new Timer((int) fpsToMsPF(10), e ->
+            {
+                if (++index >= sprites.length)
+                    index = 0;
+
+                comp.repaint();
+            });
+
+            t.start();
         });
     }
 }
